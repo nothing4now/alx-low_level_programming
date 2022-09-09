1287cc5a22bd740b95a7111ce9ed008d006790f5
yusuf <yusufkolu12345@gmail.com>
2022-09-09 11:11:41 -0700

    alx

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: print all aplhabet letters
+ * Description: print all aplhabet letters except q and e
  *
  * Return: Always 0 (Success)
 */
@@ -14,10 +14,11 @@ int main(void)
 
 	while (ch <= 'z')
 	{
+		if (ch == 'e' || ch == 'q')
+			++ch;
 		putchar(ch);
 		++ch;
 	}
 	putchar('\n');
 
 	return (0);
-}
