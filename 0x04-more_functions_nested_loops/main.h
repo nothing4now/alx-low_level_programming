02faff8ed2d603ffc60dd11fbe103be313cc12e3
yusuf <yusufkolu12345@gmail.com>
2022-09-17 08:39:19 -0700

    fixed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,34 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-
-/**
- * _isupper - checks for uppercase
- * @c: case
- * Return: sucess
-*/
 int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+
 int _isupper(int c);
+
 int _isdigit(int c);
-void print_number(int n);
-void print_triangle(int size);
-void print_square(int size);
-void print_diagonal(int n);
-void print_line(int n);
-void more_numbers(void);
-void print_most_numbers(void);
+
 int mul(int a, int b);
-#endif /* MAIN_H */
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+int isPrime(long int n);
+
+long int biggestFactor(long int a);
+
+void print_number(int n);
+
+int numDigits(int n);
+#endif
